af4340909778af4853379ece61137d5934bf5bce
YSongYS <songyishu.yang@gmail.com>
2019-01-09 11:59:36 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,21 @@
-require 'pry'
 
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    sentence_ending = [". ","? ","! "]
+    count = self.split(Regexp.union(sentence_ending)).length
+    count
   end
 end
