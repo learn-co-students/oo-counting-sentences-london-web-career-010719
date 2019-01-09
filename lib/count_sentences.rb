a8430c899e74ec2f5875e8a4ad690fb1084f357d
Minha Koo <kkooming2@gmail.com>
2019-01-09 18:49:27 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,35 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    self.split.select do |word|
+      word.sentence? || word.question? || word.exclamation?
+    end.count
   end
+  # # # binding.pry
+  
+  # def count_sentences
+  #   self.split.select do |word|
+  #     word.include?(".") || ("?") || ("!")
+  #   end.count
+  # end
+  # def count_sentences
+  #   self.split(/[!?.]/).count
+  #   binding.pry
+  # end
+  
+  
 end
+
+
